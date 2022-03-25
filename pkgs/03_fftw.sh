cc342dc45d758f8f44ade46dc0cb00c4be85e34f
Theodore Kisner <tskisner.public@gmail.com>
2022-03-24 22:00:10 -0700

    Restore fftw package, since we need the single precision lib

---
+++ pkgs/03_fftw.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+pkg="fftw"
+version=3.3.10
+psrc=${pkg}-${version}
+pfile=${psrc}.tar.gz
+
+fetched=$(eval "@TOP_DIR@/tools/fetch_check.sh" http://www.fftw.org/${pfile} ${pfile})
+
+if [ "x${fetched}" = "x" ]; then
+    echo "Failed to fetch ${pkg}" >&2
+    exit 1
+fi
+
+if [ "@DOCKER@" = "yes" ]; then
+    log=/dev/stderr
+else
+    log="../log_${pkg}"
+fi
+
+echo "Building ${pkg}..." >&2
+
+rm -rf ${psrc}
+tar xzf ${fetched} \
+    && cd ${psrc} \
+    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure \
+    --prefix="@PREFIX@" --enable-openmp --enable-single --enable-shared > ${log} 2>&1 \
+    && make -j @MAKEJ@ >> ${log} 2>&1 \
+    && make install >> ${log} 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to build ${pkg}" >&2
+    exit 1
+fi
+
+echo "Finished building ${pkg}" >&2
+exit 0
